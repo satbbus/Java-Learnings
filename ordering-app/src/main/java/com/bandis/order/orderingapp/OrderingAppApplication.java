ba3f9269ebdff8f4f5e7a2ee502adbb8e05af45a
Chity <Chity@192.168.1.28>
2020-05-13 12:15:07 -0400

    first commit

---
+++ ordering-app/src/main/java/com/bandis/order/orderingapp/OrderingAppApplication.java
@@ -0,0 +1,13 @@
+package com.bandis.order.orderingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingAppApplication.class, args);
+	}
+
+}
---
+++ ordering-app/src/test/java/com/bandis/order/orderingapp/OrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandis.order.orderingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
